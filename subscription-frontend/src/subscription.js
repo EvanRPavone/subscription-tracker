9e4776c5483a8dd77d6150e71d739d82fe7f7946
EvanRPavone <evanrpavone@gmail.com>
2021-01-25 12:27:50 -0500

    Subscription constructor created

---
+++ subscription-frontend/src/subscription.js
@@ -0,0 +1,28 @@
+class Subscription {
+    static all = []
+    static subscriptionContainer = document.getElementById('subscription-container')
+
+    constructor({id, name, price, paymentDate, plan}) {
+        this.id = id
+        this.name = name
+        this.price = price
+        this.paymentDate = paymentDate
+        this.plan = plan
+
+        this.main = document.createElement('div')
+        this.main.id = `subs-${this.id}`
+        this.details = document.createElement('div')
+        this.details.id = `subs-${this.id}-details`
+        this.categories = document.createElement('div')
+        this.categories.id = `subs-${this.id}-categories`
+        this.editButton = document.createElement('button')
+        this.editButton.innerText = "Edit Subscription"
+        this.main.append(this.details, this.categories, this.editButton)
+
+        this.form = document.createElement('form')
+
+        this.editButton.addEventListener('click', this.renderEditSubscriptionForm)
+        this.form.addEventListener('submit', this.submitEditSubscriptionForm)
+        Subscription.all.push(this)
+    }
+}
